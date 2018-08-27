2ddf55e15c7ef249c45a8be5caf6a31465f61ac4
Aaron Parsons <aparsons@berkeley.edu>
2018-08-27 15:55:09 -0700

    Added reading webcam functionality to findcard

--- findcard.py
+++ findcard.py
@@ -1,14 +1,19 @@
-import cv2, imutils
+import cv2, imutils, os
 
 CASCADE_MTG_BACK = 'data/cascades/cascade_mtg_back.xml'
 W,H = 48,36
 
 MTG_BACK_CLASSIFIER = cv2.CascadeClassifier(CASCADE_MTG_BACK)
 
-def find(filename, classifier=MTG_BACK_CLASSIFIER, min_size=(W,H), scale_factor=1.1, min_neighbors=1):
-    im = cv2.imread(filename)
-    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    cards = classifier.detectMultiScale(im, scaleFactor=scale_factor, 
+def read_webcam(to_file, brightness=20):
+    cmd = 'fswebcam -s brightness=%d -q --no-banner --background %s' % (brightness, to_file)
+    os.system(cmd)
+    im = cv2.imread(to_file)
+    return im
+
+def find(im, classifier=MTG_BACK_CLASSIFIER, min_size=(W,H), scale_factor=1.1, min_neighbors=1):
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    cards = classifier.detectMultiScale(gray, scaleFactor=scale_factor, 
                 minNeighbors=min_neighbors, minSize=min_size, flags=cv2.CASCADE_SCALE_IMAGE)
     centers = [(x+w/2,y+h/2) for (x,y,w,h) in cards]
     return centers
