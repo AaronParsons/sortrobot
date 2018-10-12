35e2925ce1f8dd6ee52dc18e12a2e178a56fae8f
Aaron Parsons <aparsons@berkeley.edu>
2018-10-12 15:31:34 -0700

    Added a fitting of a v002 of the neural net, which had a smaller fully connected layer to avoid huge file sizes.

--- sortrobot/neural.py
+++ sortrobot/neural.py
@@ -16,7 +16,7 @@ def conv2d(x, W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-# VERSION 001
+# VERSION 002
 
 HALF_SZ = 32
 IMG_SIZE = HALF_SZ * 2
@@ -51,8 +51,8 @@ h_pool2 = max_pool_2x2(h_conv2)
 # layer with 1024 neurons to allow processing on the entire image. We reshape 
 # the tensor from the pooling layer into a batch of vectors, multiply 
 # by a weight matrix, add a bias, and apply a ReLU.
-W_fc1 = weight_variable([16 * 16 * 64, 1024])
-b_fc1 = bias_variable([1024])
+W_fc1 = weight_variable([16 * 16 * 64, 128])
+b_fc1 = bias_variable([128])
 
 h_pool2_flat = tf.reshape(h_pool2, [-1, 16 * 16 * 64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
@@ -66,14 +66,13 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_fc2 = weight_variable([1024, 2])
+W_fc2 = weight_variable([128, 2])
 b_fc2 = bias_variable([2])
 
 h_fc = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 y_conv = h_fc
 
 # Training interface
-
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
