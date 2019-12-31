9a16476c432031e9fe4288651b937a5c2944c007
Aaron Parsons <aparsons@berkeley.edu>
2019-12-30 16:38:45 -0800

    First update to new card-feed mechanism

--- sortrobot/mech.py
+++ sortrobot/mech.py
@@ -18,13 +18,13 @@ except(ImportError):
             pass
 
 # SPEED CONSTANTS
-DN_SPEED = 0.965
+DN_SPEED = 1. # 0.965
 UP_SPEED = 1.
 LIFT_TIME = 0.33
 SLIDE_LF_SPEED = 1.
-SLIDE_RT_SPEED = 0.9625
+SLIDE_RT_SPEED = 1. # 0.9625
 SLIDE_TIME = 0.1125
-SLIDE_POLY = numpy.array([1.07980235e-04,-1.45621868e-03,1.98557185e-03,5.55231344e-01,2.23568309e-01])
+#SLIDE_POLY = numpy.array([1.07980235e-04,-1.45621868e-03,1.98557185e-03,5.55231344e-01,2.23568309e-01])
 
 # DEFAULT POSITIONS
 HEIGHT = 4
@@ -81,20 +81,8 @@ class Robot:
                 pass
         # After clearing all threads, re-enable access
         self._stop_event.clear()
-    def valve_close(self):
-        '''Close the valve, which propagates vacuum to the cup.'''
-        self._oc_cmd(oc1=0)
-    def valve_open(self):
-        '''Open the valve, which breaks the vacuum.'''
-        self._oc_cmd(oc1=1)
-    def pump_on(self):
-        '''Turn on the pump to generate suction.'''
-        self._oc_cmd(oc2=1)
-    def pump_off(self):
-        '''Turn on the pump to stop generating suction.'''
-        self._oc_cmd(oc2=0)
-    def up(self, distance, block=True):
-        '''Move the robot arm up the specified distance.
+    def fd(self, distance, block=True):
+        '''Feed a card forward the specified distance.
         Arguments:
             distance: nominally inches of displacment, but not accurate.
             block: if True, wait until motion completes before returning,
@@ -111,8 +99,8 @@ class Robot:
         thd.start()
         if block: thd.join()
         else: return thd
-    def dn(self, distance, block=True):
-        '''Move the robot arm down the specified distance.
+    def bk(self, distance, block=True):
+        '''Pull a card back the specified distance.
         Arguments:
             distance: nominally inches of displacment, but not accurate.
             block: if True, wait until motion completes before returning,
@@ -137,7 +125,7 @@ class Robot:
                 otherwise return a handle to the thread in charge of stopping motion.
         Returns:
             None, unless block is False, then Thread handle.'''
-        dt = numpy.polyval(SLIDE_POLY, distance) * SLIDE_TIME
+        dt = distance * SLIDE_TIME
         if self.verbosity >= 1: print('LF:', dt)
         def lf_thread():
             self._motor_cmd(m2=-SLIDE_LF_SPEED)
@@ -155,7 +143,7 @@ class Robot:
                 otherwise return a handle to the thread in charge of stopping motion.
         Returns:
             None, unless block is False, then Thread handle.'''
-        dt = numpy.polyval(SLIDE_POLY, distance) * SLIDE_TIME
+        dt = distance * SLIDE_TIME
         if self.verbosity >= 1: print('RT:', dt)
         def rt_thread():
             self._motor_cmd(m2=SLIDE_RT_SPEED)
@@ -165,79 +153,28 @@ class Robot:
         thd.start()
         if block: thd.join()
         else: return thd
-    #def accel_lf(self, dt, dV_dt=4., Vmin=.4, update=.05):
-    #    V = Vmin
-    #    t0 = time.time()
-    #    while True:
-    #        V = min(1., V + dV_dt * update)
-    #        print time.time()-t0, dt, V, dt - V / dV_dt
-    #        self._motor_cmd(m2=-V)
-    #        time.sleep(update)
-    #        if time.time() - t0 > dt - (V-Vmin) / dV_dt: break
-    #    while True:
-    #        V = max(Vmin, V - dV_dt * update)
-    #        print time.time() - t0, dt, V
-    #        self._motor_cmd(m2=-V)
-    #        if V <= Vmin: break
-    #        time.sleep(update)
-    #    self._motor_cmd(m2=0)
-    def grab(self):
-        '''Turn the pump on and close the valve to grab a card.'''
-        self.pump_on()
-        self.valve_close()
-    def release(self, dt=.5, block=False):
-        '''Break the vacuum seal to drop the card.
-        Arguments:
-            dt: how long to release the card before reestablishing vacuum.
-            block: if True, wait until motion completes before returning,
-                otherwise return a handle to the thread in charge of stopping motion.
-        Returns:
-            None, unless block is False, then Thread handle.'''
-        def release_thread():
-            self.valve_open() # release pressure
-            time.sleep(dt)
-            self.valve_close() # stop wasting valve current
-        thd = threading.Thread(target=release_thread)
-        thd.start()
-        if block: thd.join()
-        else: return thd
     def home(self, pos=POS2, hgt=HEIGHT):
-        '''Move robot arm up and slide tray all the way to the left.'''
-        self.up(hgt)
+        '''Move slide tray all the way to the left.'''
         self.lf(pos + 0.25)
-    def carry_card(self, pos=POS2, hgt=HEIGHT):
-        '''Assuming you are HOME, pick up a card and move it to a new stack.
+    def feed_card(self, FD=4.5, BK=2):
+        '''Push a card off the stack and pull the next one back in.
         Arguments:
-            pos: the position to the right of the new stack.  Default POS2.
-            hgt: the height of the robot arm in the up position.  Default HEIGHT.'''
-        self.grab()
-        self.dn(hgt)
-        up_thd = self.up(hgt, block=False)
-        # Shimmy to shake off cards
-        self.rt(.1); self.lf(.1)
-        self.rt(.1); self.lf(.1)
-        self.rt(.1); self.lf(.1)
-        up_thd.join()
-        self.rt(pos)
-        self.dn(hgt)
-        self.release()
-        self.pump_off()
-    def move_card(self, pos=POS2, hgt=HEIGHT):
-        '''Fron HOME, pick up a card, move it to a new stack, and return HOME.
-        Arguments:
-            pos: the position to the right of the new stack.  Default POS2.
-            hgt: the height of the robot arm in the up position.  Default HEIGHT.'''
-        self.carry_card(pos=pos, hgt=hgt)
-        self.home(pos=pos, hgt=hgt)
-    def take_pic(self, shift=POS2):
+            FD: how much to feed forward
+            BK: how much to pull back'''
+        self.fd(FD)
+        self.bk(BK)
+    def take_pic(self):
         '''Take a picture from the webcam and return the image as a numpy array.
         Arguments:
             shift: the distance to move the arm right out of the way before taking
                 the pic.  Default POS2.
         Returns:
             im: the image as a numpy array.'''
-        self.rt(shift)
         self.filename, im = webcam.read().items()[0]
         if self.verbosity >= 1: print('Webcam:', self.filename)
-        self.lf(shift)
         return im
+    def get_card_index(self):
+        '''Return a picture of the top (or bottom) slice of the card.'''
+        im = self.take_pic()
+        index = im[50:620,960:1040]
+        return index
