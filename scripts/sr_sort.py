7c718f7cc2c2e7ed99981ef4dde30039337bb087
Aaron Parsons <aparsons@berkeley.edu>
2020-05-18 01:40:19 +0100

    Optimizations and bugfixes on rpi

--- scripts/sr_sort.py
+++ scripts/sr_sort.py
@@ -1,4 +1,4 @@
-from sortrobot.sort import Robot
+from sortrobot.mech import Robot
 from sortrobot.webcam import Camera
 from sortrobot.neural import Classifier
 from PIL import Image
@@ -21,23 +21,27 @@ RESULTS = {
     1: 'front',
 }
 
+curpos = None
+
 for i in range(num):
     filebase = random_filename()
     filename = os.path.join(directory, filebase)
     print('%d/%d scanning -> %s' % (i, num, filename))
     cam.rgb_to_file(filename)
     im = Image.open(filename)
-    prediction = c.classify(im)
+    prediction = classifier.classify(im)
     r = RESULTS[int(prediction[0])]
-    print('%d/%d classfied as %s' % (r))
+    print('      classfied as %s' % (r))
     new_directory = os.path.join(directory, r)
     if not os.path.exists(new_directory):
         os.mkdir(new_directory)
-    print('%d/%d moving to %s' % (new_directory))
+    print('      moving to %s' % (new_directory))
     os.rename(filename, os.path.join(new_directory, filebase))
-    if r == 'back':
-        sr.lf(9)
-    else:
-        sr.rt(9)
+    if curpos != r:
+        if r == 'back':
+            sr.lf(1.1)
+        else:
+            sr.rt(1.1)
+    curpos = r
     sr.feed_card()
 
