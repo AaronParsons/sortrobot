68473cc30e6df50273d619bb9a1e83b6417f2464
Aaron Parsons <aparsons@berkeley.edu>
2020-05-17 19:16:42 -0700

    Fixed typo in filename generation in sr_sort

--- scripts/sr_sort.py
+++ scripts/sr_sort.py
@@ -5,7 +5,7 @@ import numpy as np
 from PIL import Image
 import sys, random, os
 
-def random_filename(lim=2**13):
+def random_filename(lim=2**31):
     hexstr = hex(random.randint(0,lim))[2:]
     hexstr = ('0' * 8 + hexstr)[-8:]
     return hexstr + '.jpg'
